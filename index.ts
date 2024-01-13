03cd7e119daa0dca6ff4d408eb2c66584007a014
yuan deson <42114557@qq.com>
2024-01-13 16:41:32 +0800

    fix

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 type Methods = 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE';
-type DataType = RequestInit['body']|Record<any,any>;
+type DataType = RequestInit['body']|Record<any,any>|number;
 type HeaderType = Record<string, string>;
 type IFetchOption = Omit<RequestInit, "body" | "method" | "headers">
 type ResponseType = "json" | "text" | "formData" | "blob" | "arrayBuffer"
@@ -127,9 +127,9 @@ class Request {
       }
     }
     if(data){
-      const dataIsString = typeof data === "string";
-      if (dataIsString || typeof data === "number") {
-        url += `/${removeSlash(data + '')}`; //拼接url
+      const dataIsString = typeof data === "string"||typeof data === "number";
+      if (dataIsString) {
+        url += `/${removeSlash(data.toString())}`; //拼接url
         bodyHandler(body)
       }else{
         bodyHandler(data)
